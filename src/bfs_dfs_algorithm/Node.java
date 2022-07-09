e1fdfc4b227ad6444919a824a2f61d198b0d4c35
j43lani <mjfbogor@gmail.com>
2022-07-09 16:05:19 +0700

    intial commit ke 2

--- src/bfs_dfs_algorithm/Node.java
+++ src/bfs_dfs_algorithm/Node.java
@@ -1,11 +1,23 @@
 package bfs_dfs_algorithm;
 
 public class Node {
-    Node left;
-    Node right;
+    public Node left;
+    public Node right;
     String data;
+    int value;
     public Node(String data){
         this.data = data;
+    }
+    public Node(int value){
+        this.value = value;
+    }
+    public int getValue(){
+        return this.value;
+    }
+    public Node(int value, Node left, Node right){
+        this.value = value;
+        this.left = left;
+        this.right =right;
     }
     public Node(String data, Node left, Node right){
         this.data = data;
--- src/binarysearchtree/BinarySearchTree.java
+++ src/binarysearchtree/BinarySearchTree.java
@@ -6,13 +6,31 @@ public class BinarySearchTree {
 	private int index = 0;
 	public static void main(String[] args) {
 		BinarySearchTree bst = new BinarySearchTree();
-		bst.addNode(new Node(5));
+		/*bst.addNode(new Node(5));
 		bst.addNode(new Node(4));
 		bst.addNode(new Node(6));
 		bst.addNode(new Node(2));
-		
 		System.out.println(bst.searchVal(bst.root,1));
+		*/
+		System.out.println(bst.cariValue(bst.bikinNode(),7));
+
 	}
+//	-------------------------------------------------------------------------------------
+	bfs_dfs_algorithm.Node bikinNode(){
+		bfs_dfs_algorithm.Node rightNode = new bfs_dfs_algorithm.Node(6,new bfs_dfs_algorithm.Node(2),null);
+		bfs_dfs_algorithm.Node akar = new bfs_dfs_algorithm.Node(5,new bfs_dfs_algorithm.Node(4),rightNode);
+		return akar;
+	}
+	boolean cariValue(bfs_dfs_algorithm.Node root, int value){
+		if(root==null) return false;
+		if(root.getValue()==value) return true;
+		if(root.getValue()>value){
+			return cariValue(root.left,value);
+		}else{
+			return cariValue(root.right,value);
+		}
+	}
+//	--------------------------------------------------------------------------------------
 	void addNode(Node node) {
 		if(root==null) {
 			root = node;
